0d85368f84dd24c98cc311b595798b4bded3d20e
sigma <zhaopengwei@csii.com.cn>
2018-04-09 10:47:36 +0800

    first commit

---
+++ MiniPrograms/app.js
@@ -0,0 +1,13 @@
+var wilddog = require('wilddog-weapp-all');
+var gauge = require('gauge-min');
+
+App({
+  onLaunch: function () {
+    // var config = {
+    //   syncURL: 'https://wd2397396495eluilt.wilddogio.com/'
+    // };
+    // wilddog.initializeApp(config);
+    // this.ref = wilddog.sync().ref('Switch');
+    // console.log(gauge);
+  }
+});
